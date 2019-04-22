8b033e9454125aff4142fcd2ddbf9d03e5e4c849
pp2pp <pp2pp@gmail.com>
2019-04-22 17:56:03 +0800

    要提交的变更：    （使用 "git reset HEAD <file>..." 撤出暂存区）
    	修改：      src/simplelog.c
    	删除：      src/test.c
    
     未跟踪的文件:
       （使用 "git add <file>..." 以包含要提交的内容）
    
    	CMakeLists.txt
    	src/CMakeLists.txt
    	test/

--- src/simplelog.c
+++ src/simplelog.c
@@ -2,4 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+void _log( char *source, int line, char *fmt, ...)
+{
+	char time[64]={0};
+	struct timeval tv;
 
+	gettimeofdat(&tv, NULL);
+
+	strcpy(time, ctime(tv.tv_sec));
+	va_list ap;
+	va_start(ap, fmt);
+	fprintf(gFP, "%06d,%10.10s,%04d    ",
+			_gLOGOP.pid,  source, line);
+	vfprintf(gFP, fmt, ap);
+	va_end(ap);
+	fflush(gFP);
+}                         
