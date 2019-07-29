9033a84d884be3cbc00c30b258da2187c6f51f59
root <root@localhost.localdomain>
2019-07-29 18:38:52 +0800

    提交者：root <root@localhost.localdomain>
     位于分支 master
     您的分支与上游分支 'origin/master' 一致。
    
     要提交的变更：
    	新文件：   CMakeCache.txt
    	新文件：   CMakeFiles/2.8.12.2/CMakeCCompiler.cmake
    	新文件：   CMakeFiles/2.8.12.2/CMakeCXXCompiler.cmake
    	新文件：   CMakeFiles/2.8.12.2/CMakeDetermineCompilerABI_C.bin
    	新文件：   CMakeFiles/2.8.12.2/CMakeDetermineCompilerABI_CXX.bin
    	新文件：   CMakeFiles/2.8.12.2/CMakeSystem.cmake
    	新文件：   CMakeFiles/2.8.12.2/CompilerIdC/CMakeCCompilerId.c
    	新文件：   CMakeFiles/2.8.12.2/CompilerIdC/a.out
    	新文件：   CMakeFiles/2.8.12.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
    	新文件：   CMakeFiles/2.8.12.2/CompilerIdCXX/a.out
    	新文件：   CMakeFiles/CMakeDirectoryInformation.cmake
    	新文件：   CMakeFiles/CMakeOutput.log
    	新文件：   CMakeFiles/Makefile.cmake
    	新文件：   CMakeFiles/Makefile2
    	新文件：   CMakeFiles/Progress/1
    	新文件：   CMakeFiles/Progress/count.txt
    	新文件：   CMakeFiles/TargetDirectories.txt
    	新文件：   CMakeFiles/cmake.check_cache
    	新文件：   CMakeFiles/progress.marks
    	新文件：   CMakeLists.txt
    	新文件：   Makefile
    	新文件：   cmake_install.cmake
    	新文件：   src/CMakeFiles/CMakeDirectoryInformation.cmake
    	新文件：   src/CMakeFiles/SimpleLog.dir/C.includecache
    	新文件：   src/CMakeFiles/SimpleLog.dir/DependInfo.cmake
    	新文件：   src/CMakeFiles/SimpleLog.dir/build.make
    	新文件：   src/CMakeFiles/SimpleLog.dir/cmake_clean.cmake
    	新文件：   src/CMakeFiles/SimpleLog.dir/depend.internal
    	新文件：   src/CMakeFiles/SimpleLog.dir/depend.make
    	新文件：   src/CMakeFiles/SimpleLog.dir/flags.make
    	新文件：   src/CMakeFiles/SimpleLog.dir/link.txt
    	新文件：   src/CMakeFiles/SimpleLog.dir/progress.make
    	新文件：   src/CMakeFiles/progress.marks
    	新文件：   src/CMakeLists.txt
    	新文件：   src/Makefile
    	新文件：   src/cmake_install.cmake
    	新文件：   test/CMakeFiles/CMakeDirectoryInformation.cmake
    	新文件：   test/CMakeFiles/demo.dir/C.includecache
    	新文件：   test/CMakeFiles/demo.dir/DependInfo.cmake
    	新文件：   test/CMakeFiles/demo.dir/build.make
    	新文件：   test/CMakeFiles/demo.dir/cmake_clean.cmake
    	新文件：   test/CMakeFiles/demo.dir/depend.internal
    	新文件：   test/CMakeFiles/demo.dir/depend.make
    	新文件：   test/CMakeFiles/demo.dir/flags.make
    	新文件：   test/CMakeFiles/demo.dir/link.txt
    	新文件：   test/CMakeFiles/demo.dir/progress.make
    	新文件：   test/CMakeFiles/progress.marks
    	新文件：   test/CMakeLists.txt
    	新文件：   test/Makefile
    	新文件：   test/cmake_install.cmake
    	新文件：   test/test.c

---
+++ src/CMakeFiles/SimpleLog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SimpleLog.dir/simplelog.c.o"
+  "libSimpleLog.pdb"
+  "libSimpleLog.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/SimpleLog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
