255c9a3d032e570d7c5ae93069d80a4d608a7c20
root <root@localhost.localdomain>
2019-07-29 18:24:19 +0800

    请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交  说明将会终止提交。
     提交者：root <root@localhost.localdomain>
    
     位于分支 master
     您的分支领先 'origin/master' 共 1 个提交。
       （使用 "git push" 来发布您的本地提交）
    
     要提交的变更：
    	修改：     src/simplelog.c
    
     未跟踪的文件:
    	CMakeCache.txt
    	CMakeFiles/
    	CMakeLists.txt
    	Makefile
    	cmake_install.cmake
    	src/CMakeFiles/
    	src/CMakeLists.txt
    	src/Makefile
    	src/cmake_install.cmake
    	test/

--- src/simplelog.c
+++ src/simplelog.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "simple_log.h"
 
+PLOG * CreateLogSpace()
+{
+	PLOG  *gplog = NULL;
+	gplog =(PLOG *)calloc(1,sizeof(PLOG));
+	if (gplog == NULL)
+	{
+		printf(stderr, "CreateLogSpace error:[%d:%s]",
+				errno, strerror(errno) );
+		return NULL;
+	}
+	return gplog;
+}
 void _log( char *source, int line, char *fmt, ...)
 {
 	char time[64]={0};
